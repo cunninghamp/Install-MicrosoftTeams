f26cd9ba13ad11ab016ae1f18d362431260b6bdf
Paul Cunningham <paul@locklan.com.au>
2017-03-15 13:41:57 +1000

    Initial version

---
+++ Install-MicrosoftTeams.ps1
@@ -0,0 +1,95 @@
+﻿<#
+.SYNOPSIS
+Install-MicrosoftTeams.ps1 - Microsoft Teams Desktop Client Deployment Script
+
+.DESCRIPTION 
+This PowerShell script will silently install the Microsoft Teams desktop client.
+
+The Teams client installer can be downloaded from Microsoft:
+https://teams.microsoft.com/downloads
+
+.PARAMETER SourcePath
+Specifies the source path for the Microsoft Teams installer.
+
+
+.EXAMPLE
+.\Install-MicrosoftTeams.ps1 -Source \\mgmt\Installs\MicrosoftTeams
+
+Installs the Microsoft Teams client from the Installs share on the server MGMT.
+
+.NOTES
+Written by: Paul Cunningham
+
+Find me on:
+
+* My Blog:	http://paulcunningham.me
+* Twitter:	https://twitter.com/paulcunningham
+* LinkedIn:	http://au.linkedin.com/in/cunninghamp/
+* Github:	https://github.com/cunninghamp
+
+For more Office 365 tips, tricks and news
+check out Practical 365.
+
+* Website:	http://practical365.com
+* Twitter:	http://twitter.com/practical365
+
+Change Log
+V1.00, 15/03/2017 - Initial version
+#>
+
+#requires -version 4
+
+
+[CmdletBinding()]
+param (
+
+	[Parameter(Mandatory=$true)]
+	[string]$SourcePath
+
+)
+
+
+function DoInstall {
+
+    $Installer = "$($SourcePath)\Teams_windows_x64.exe"
+
+    If (!(Test-Path $Installer)) {
+        throw "Unable to locate Microsoft Teams client installer at $($installer)"
+    }
+
+    Write-Host "Attempting to install Microsoft Teams client"
+
+    try {
+        $process = Start-Process -FilePath "$Installer" -ArgumentList "-s" -Wait -PassThru -ErrorAction STOP
+
+        if ($process.ExitCode -eq 0)
+        {
+            Write-Host -ForegroundColor Green "Microsoft Teams setup started without error."
+        }
+        else
+        {
+            Write-Warning "Installer exit code  $($process.ExitCode)."
+        }
+    }
+    catch {
+        Write-Warning $_.Exception.Message
+    }
+
+}
+
+#Check if Office is already installed, as indicated by presence of registry key
+$installpath = "$($env:LOCALAPPDATA)\Microsoft\Teams"
+
+if (-not(Test-Path "$($installpath)\Update.exe")) {
+    DoInstall
+}
+else {
+    if (Test-Path "$($installpath)\.dead") {
+        Write-Host "Teams was previously installed but has been uninstalled. Will reinstall."
+        DoInstall
+    }
+}
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Install-MicrosoftTeams
-PowerShell Script for Microsoft Teams Client Deployment
+# PowerShell Script for Microsoft Teams Client Deployment
+
+Install-MicrosoftTeams.ps1 is a PowerShell script for automating the deployment of the Microsoft Teams desktop client for Windows.
+
+### Instructions
+
+1. Download the Teams client installer from [Microsoft](https://teams.microsoft.com/downloads).
+2. Run the script and specify the **-SourcePath** for the installer.
+
+#### Example:
+
+```
+.\Install-MicrosoftTeams.ps1 -Source \\mgmt\Installs\MicrosoftTeams
+```
+
+Using the example command above, the script will look in \\mgmt\installs\MicrosoftTeams for the Teams_Windows_x64.exe file, and launch a silent installation of the Teams client.
+
+### Credits
+
+Written by: 
+
+- Paul Cunningham, [Blog](http://practical365.com) | [GitHub](https://github.com/cunninghamp) | [Twitter](https://twitter.com/paulcunningham) 
+
+For more Office 365 tips, tutorials, and news check out [Practical 365](http://practical365.com).
